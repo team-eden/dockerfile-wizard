d688688656221efe83aebabd7f456ae20d14999f
Bruz Marzolf <bruz@bruzilla.com>
2018-05-30 10:55:53 -0700

    Configure for running Expo builds

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,6 +4,8 @@ echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
 
 echo "RUN apt-get update"
 
+echo "RUN apt-get install expect -y"
+
 if [ ! -e $RUBY_VERSION_NUM ] ; then
     echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
     tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
